7ff6f7d90238d5e5ef6e6cc0b76ac2c2202ab4c4
karlebh <akejucaleb@gmail.com>
2021-04-27 20:11:35 +0100

    responsive bug fixes

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -3,13 +3,12 @@
         <!-- Mobile Header -->
         <div class="lg:hidden bg-m-image-hero bg-cover" style="height: 100rem;">
             <div class="mx-10 pt-20 flex items-center justify-between text-gray-100">
-                <!-- <h1 class="font-bold sm:font-medium font-3xl">loopstudios</h1> -->
                 <logo />
                 <svg v-if="isOpen" @click="isOpen = ! isOpen" class="cursor-pointer" width="24" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fill-rule="evenodd"><path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z"/></g></svg>
             </div>
 
             <div class="h-full flex flex-wrap content-center justify-center">
-                <p class="text-gray-100 font-light border-4 text-5xl sm:text-2xl md:text-6xl p-5 sm:text-7xl sm:p-12 md:text-8xl md:p-9">
+                <p class="text-gray-100 font-light border-4 text-5xl sm:text-4xl md:text-6xl p-5 sm:text-7xl sm:p-10 md:text-8xl md:p-9">
                     IMMERSIVE <br>
                     EXPERIENCES <br>
                     THAT <br>
@@ -20,8 +19,7 @@
             <!-- Nav Menu -->
             <nav v-if="!isOpen" 
             class="px-10 bg-black h-full fixed right-0 left-0 bottom-0 top-0 text-white">
-                <div class="mr-10 pt-20 flex items-baseline justify-between text-gray-100">
-                    <!-- <h1 class="font-bold sm:font-medium text-3xl sm:text-6xl">loopstudios</h1> -->
+                <div class="mr-10 pt-20 flex items-center justify-between text-gray-100">
                     <logo />
                     <svg @click="isOpen = ! isOpen" class="cursor-pointer" width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z" fill="#FFF" fill-rule="evenodd"/></svg>
 
